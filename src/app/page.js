37db66792724604e02983193443f9f6dc6eb1ec9
Johnny <joaogbdesouza@gmail.com>
2025-01-15 23:38:08 -0300

    dealing with responsivity

--- src/app/page.js
+++ src/app/page.js
@@ -26,19 +26,19 @@ export default function Home() {
   const [shortUrl, setShortUrl] = useState(null)
   const [url, setUrl] = useState("")
   return (
-    <div className="container mx-auto min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="text-center">
+    <div className="container mx-auto sm:w-full sm:p-0 font-[family-name:var(--font-geist-sans)]">
+      <main className="text-center sm:p-0">
         <h1 className="font-bold p-5 text-2xl">URL SHORTENER</h1>
-        <div className="p-10 w-10/12 mx-auto bg-slate-100 shadow-lg">
+        <div className="p-10 w-12/12 lg:w-10/12 mx-auto bg-slate-100 shadow-lg">
           <form method="POST" onSubmit={create}>
             <label className="text-lg">Paste here the url to be shortened</label>
-            <input name="url" value={url} onChange={(e) => setUrl(e.target.value)} className="w-10/12 border rounded-lg border-blue-300 outline-blue-500 bg-white p-5 text-center" placeholder="https://url.here" />
-            <input className="w-36 border rounded-lg p-5 mt-5 hover:bg-blue-300 hover:cursor-pointer bg-white font-bold" type="submit" value="Short url, plz" />
+            <input name="url" value={url} onChange={(e) => setUrl(e.target.value)} className="w-full lg:w-10/12 border rounded-lg border-blue-300 outline-blue-500 bg-white p-5 text-center" placeholder="https://url.here" />
+            <input className="w-full lg:w-36 border rounded-lg p-5 mt-5 hover:bg-blue-300 hover:cursor-pointer bg-white font-bold" type="submit" value="Short url, plz" />
           </form>
           {
             shortUrl ?
               <div>
-                <p className="bg-green-300 rounded-lg p-5 mt-5 font-bold text-lg">Your short url is {process.env.NEXT_PUBLIC_BACKEND_URL}/{shortUrl}</p>
+                <p className="bg-green-300 md:w-full rounded-lg p-5 mt-5 font-bold text-lg">Your short url is {process.env.NEXT_PUBLIC_BACKEND_URL}/{shortUrl}</p>
               </div>
               : ""
           }
